7c7d1ac8516c6dd07cecec86bc1d0147566a9b67
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-08-18 22:20:35 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,6 +1,29 @@
 module.exports = {
-    token: '5776057284:AAF_dXoL1dZUnVINe1CDwe9DdYurC29xqnI',
-    admins: [895687774],
-    curr:'INR'
-}
 
+    mongoLink:'mongodb+srv://Alwayshelpeper56@cluster0.n6rlw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+    
+    bot_token:'5776057284:AAF_dXoL1dZUnVINe1CDwe9DdYurC29xqnI',//bot token
+    
+    bot_name:'ImpulseXAirdrops_Bot',//bot username without @
+    
+    bot_admin:1680884053,// admin telegram id
+    
+    channelsList : ['@Helpeveryone5','@Facinated_Airdrops','@FacinatingAirdrops','@unicorn_drops'],// channels list
+    
+    reffer_bonus:10000,// refferal bonus amount
+    
+    min_wd:10000,//min withdrawal amount
+    
+    daily_bonus:100000,// daily bonus amount
+    
+    currency:'MetaJoker', //bot cuurency
+    
+    cb_api_key:'',//coinbase api key
+    
+    cb_api_secret:'',// coinbase secret key
+    
+    cb_account_id:'',// counbase account id for the bot currency
+    
+    payment_channel:'@PayoutsHelper'// payment channel username
+    
+    }
